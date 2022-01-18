922525aad4922c74db4d1f05b83d1c270a720a08
tatarudragos <tataru.dragos@yahoo.com>
2022-01-18 21:15:32 +0200

    curs 1

---
+++ hello/hello.js
@@ -0,0 +1,13 @@
+     // ruleaza tellMe dupa 3000ms(3secunde)
+     setTimeout(tellMe, 3000)
+     
+     // creeaza o noua instructiune(functie)numita tellME
+     function tellMe() {
+        alert("Hello world!")
+        alert("Welcome to javascript")
+         }
+      
+        /*comentariu pe 
+        mai multe linii
+        */
+       // comentariu pe o linie
---
+++ variable/variable.js
@@ -0,0 +1,14 @@
+// punem data curenta in variabila date
+var date =new Date()
+
+var hour = date.getHours()
+var minutes = date.getMinutes ()
+
+var isRaining = false
+// am adaugat la un sir o variabila
+var message ="Hello! Este ora" + hour + ":" + minutes
+// o variabila fara valoare (undefined)
+var city
+
+alert(message)
+
